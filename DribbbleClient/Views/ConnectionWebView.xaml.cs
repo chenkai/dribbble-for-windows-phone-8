ffb2056b689032f44c26529715e4e0b492900dfa
chenkai <chenkaihome@live.cn>
2013-08-02 15:58:28 +0800

    fix connect bug tip

--- DribbbleClient/Views/ConnectionWebView.xaml.cs
+++ DribbbleClient/Views/ConnectionWebView.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using MoCommon;
+using DribbbleClient.Common;
 
 namespace DribbbleClient.Views
 {
@@ -25,7 +26,7 @@ namespace DribbbleClient.Views
            if (connectObj == null)
                return;
 
-          
+           this.PhoneWebBrowser_WB.Navigate(new Uri(connectObj.ToString(), UriKind.RelativeOrAbsolute));
         }
 
         string _connectType = string.Empty;
@@ -44,6 +45,20 @@ namespace DribbbleClient.Views
                     }
                 }
             }
+        }
+
+        private void PhoneWebBrowser_WB_Navigating(object sender, NavigatingEventArgs e)
+        {
+            //start process bar 
+            LoadProcessBarHelper processBarHelper = new LoadProcessBarHelper();
+            processBarHelper.StartProcessBar();
+        }
+
+        private void PhoneWebBrowser_WB_Navigated(object sender, NavigationEventArgs e)
+        {
+            //end process bar 
+            LoadProcessBarHelper processBarHelper = new LoadProcessBarHelper();
+            processBarHelper.EndProcessBar();
         }
     }
 }
